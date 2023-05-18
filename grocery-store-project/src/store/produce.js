d04a4ef33ef4b940adfef4d26cd7248f0678b2ee
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-18 17:25:32 -0400

    Create produceReducer

---
+++ grocery-store-project/src/store/produce.js
@@ -0,0 +1,23 @@
+import * as produceData from '../mockData/produce.json';
+
+export default function produceReducer(state = {}, action) {
+    switch (action.type) {
+        case POPULATE:
+            const newState = {};
+            action.produce.forEach(produce => {
+                newState[produce.id] = produce;
+            });
+            return newState;
+        default:
+            return state;
+    }
+}
+
+const POPULATE = "produce/POPULATE";
+
+export function populateProduce(produce) {
+    return {
+        type: POPULATE,
+        produce: produceData
+    }
+}
